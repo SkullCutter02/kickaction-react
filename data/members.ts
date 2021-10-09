b063788c2397891287e3a5cb2bc085d2ecfbba6a
skullcutter02 <=>
2021-10-09 12:35:41 +0800

    Added members

---
+++ data/members.ts
@@ -0,0 +1,91 @@
+const members = [
+  {
+    id: 1,
+    name: "Justin Kim",
+    role: "Headmaster & Co-founder",
+    description:
+      "Hey y'all - I'm Justin Kim! I started Kick Action with the vision of making empowerment, through the martial arts, accessible for marginalised communities. As hinted above, the martial arts is an integral part of my life: I've been internationally competing in Olympic-style Taekwondo for almost 10 years and am an active, licensed World Taekwondo Headquarters Master. Even since Jake Paul knocked Ben Askren out, I've been working on boxing too. Other than the martial arts, I love Debate, Basketball, and Taylor Swift!",
+    imageOffsetHome: "center -50px",
+    imageOffsetModal: "center -50px",
+  },
+  {
+    id: 2,
+    name: "Marco Lam",
+    role: "Co-head of Graphic Designer & Co-founder",
+    description:
+      "Hi everyone! My name is Marco. I'm the co-founder of Kick Action as well as the co-head of graphic design. I am passionate in drawing/painting as well as practicing the martial arts. I am a practitioner of Muay Thai, 散打 (Chinese kick boxing) and have begun my journey on jiujitsu as well. I will be one of the coaches teaching domestic workers martial arts. I'm looking forward to using my skill sets at Kick Action and showing everyone the value Kick Action brings!",
+  },
+  {
+    id: 3,
+    name: "Jack Liang",
+    role: "Head of Marketing",
+    description:
+      "Hello there! I'm Jack, head of marketing at Kick Action. I'm very passionate about filmmaking, and also have a strong interest in CGI and rendering stuff. I've always looked for ways to my skills in the real world and bring about change, and am very glad to be in this position. I'm looking forward to utilizing and expanding my skill sets here at Kick Action, and at the same time promote Kick Action to a wider audience.",
+    imageOffsetHome: "center 0",
+    imageOffsetModal: "center 0",
+  },
+  {
+    id: 4,
+    name: "Leo Tang",
+    role: "Head of Product Design",
+    description:
+      "I'm Leo, Graphic Designer and Head of Product Design at Kick Action HK. I'm very passionate baout design and will be providing support in terms of acquiring or creating utilitarian products used during kick Action Sessions. I hope to empower others to accomplish their goals by creating the physical objects they are in search of in order to do so.",
+  },
+  {
+    id: 5,
+    name: "Michelle Qiu",
+    role: "Co-head of Graphic Design",
+    description:
+      "Hi everyone! I'm Michelle and I do design work for Kick Action. I love illustrating and animation. I look forward to contributing my skills to help promote this project!",
+  },
+  {
+    id: 6,
+    name: "Alan Au",
+    role: "Co-head of Technology",
+    description:
+      "Hi, my name is Alan Au, and I am the Co-head of Technology at Kick Action HK, whose responsibilities cover everything related to tech, including the making of this website. I like watching movies, gaming, and coding. I also play the oboe and the drums for fun. I've been coding for one and a half year now, and web development for around a year. I'm proficient in both the frontend and the backend of web development.",
+    imageOffsetHome: "center 0px",
+    imageOffsetModal: "center -20px",
+  },
+  {
+    id: 7,
+    name: "Alvin Ng",
+    role: "Co-head of Technology",
+    description:
+      "Hello everyone! My name is Alvin Ng, and I am the co-head of technology at Kick Action. Web development and music are both big parts of my life, which is why I often spend my free time coding and designing websites or playing the violin. I am particularly passionate about front-end web development, and I hope I will be able to contribute to and promote this project through my passion for coding and web design.",
+    imageOffsetHome: "center -10px",
+    imageOffsetModal: "center -10px",
+  },
+  {
+    id: 8,
+    name: "Nathan Woo",
+    role: "Master Instructor",
+    description:
+      "Hi everyone! My name is Nathan. I'm a instructor for Kick Action. I am a practitioner of Muay Thai, Modern Mixed Martial Arts, and Brazilian Jiu Jitsu. I will be one of the instructors teaching the domestic workers martial arts and look forward to share my martial arts knowledge through Kick Action!",
+  },
+  {
+    id: 9,
+    name: "Felix Chen",
+    role: "Master Instructor",
+    description:
+      "Hi all, I’m Felix. I have been training boxing for almost three years now, and will be one of the instructors that specialize in hand-to-hand combat. I hope to share my experiences with you all through Kick Action!",
+  },
+  {
+    id: 10,
+    name: "Catrina Lee",
+    role: "Master Instructor",
+    description:
+      "Hey everyone, I’m Catrina and I’m 16 years old. I currently go to Kellett School and starting my A levels. In my free time, I like to channel my energy into sports, with running and taekwondo being the main sports I do. This summer, I have also committed to volunteering at a food bank which I have found to be an incredibly rewarding experience. However, I would like to further give back to our community and having followed Justin and Kick Action for quite a while, it would be my pleasure to be a part of this :)",
+  },
+  {
+    id: 11,
+    name: "Audrey Chan",
+    role: "Master Instructor",
+    description:
+      "Wassup yall, it's Audrey! I'm a practitioner of Muay Thai and Krav Maga. If I'm not listening to music, I'm either skating or doing other sports. I hope to share my passion for martial arts with yall through Kick Action!",
+    imageOffsetHome: "center -30px",
+    imageOffsetModal: "center -60px",
+  },
+];
+
+export default members;
--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Hero from "../components/ui/shared/Hero";
+import Members from "../components/ui/aboutUs/Members";
 import heroes from "../data/heroes";
 
 const AboutUsPage: React.FC = () => {
   return (
     <>
       <Hero heroContent={heroes.aboutUs} heroFileName={"about-us-hero.jpg"} />
+      <Members />
     </>
   );
 };
