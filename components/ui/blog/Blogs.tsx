bbe876282d0270e15eda6151461bab86db18e27e
skullcutter02 <=>
2021-10-12 22:25:01 +0800

    Styled blogs

--- components/ui/blog/Blogs.tsx
+++ components/ui/blog/Blogs.tsx
@@ -1,19 +1,50 @@
 import React from "react";
 import { useQuery } from "react-query";
+import { format, parseISO } from "date-fns";
 
 import getBlogs from "../../../queries/getBlogs";
+import useAos from "../../../hooks/useAos";
 
 const Blogs: React.FC = () => {
   const { data: blogs } = useQuery<IBlog[]>("blogs", getBlogs);
 
+  useAos({ once: true });
+
   return (
     <>
-      {blogs.map((blog) => (
-        <div className="blog" key={"blog_" + blog.id}>
-          <p>{blog.title}</p>
-          <p>{blog.body}</p>
-        </div>
-      ))}
+      <section className="blogs">
+        {blogs.map((blog) => (
+          <article className="blog" key={"blog_" + blog.id} data-aos="fade-left">
+            <p className="blog-article-date">{format(parseISO(blog.created_at), "MMM. d, yyyy")}</p>
+            <p className="blog-article-title">{blog.title}</p>
+          </article>
+        ))}
+      </section>
+
+      <style jsx>{`
+        article.blog {
+          background: #414141;
+          box-shadow: 10px 10px 20px 5px #1f1f1f;
+          border-radius: 5px;
+          padding: 30px;
+          margin: 30px 0;
+          cursor: pointer;
+        }
+
+        .blog-article-date {
+          font-style: italic;
+        }
+
+        .blog-article-title {
+          font-weight: 500;
+          white-space: nowrap;
+          overflow: hidden;
+          display: block;
+          text-overflow: ellipsis;
+          margin-top: 15px;
+          font-size: 1.1rem;
+        }
+      `}</style>
     </>
   );
 };
